15e809071e3246845652dcd71e6663af063b3ae9
edkim3275 <edkim3275@gmail.com>
2023-11-22 22:32:18 +0900

    231122_create wallet for each network - ethereum, bitcoin, solana

--- practice_project/src/ethersUtils.ts
+++ practice_project/src/ethersUtils.ts
@@ -1,6 +1,12 @@
 import { ethers } from "ethers";
 import Web3 from "web3";
-
+import * as SolanaWeb3 from "@solana/web3.js";
+import { Keypair } from "@solana/web3.js";
+import * as bip39 from "bip39";
+import * as bitcoin from "bitcoinjs-lib";
+import BIP32Factory, { BIP32Interface } from "bip32";
+import ecc from "@bitcoinerlab/secp256k1";
+import axios from "axios";
 interface EncryptedKeystoreV3JSON {
   version: number;
   id: string;
@@ -127,15 +133,81 @@ export const createWallet = (network: string, seedPhrase: string) => {
   switch (network) {
     case "ethereum":
       wallet = ethers.Wallet.fromPhrase(seedPhrase);
-      console.log("ether wallet", wallet);
-      return wallet;
+      const ethereumWallet = {
+        network: "ethereum",
+        name: "",
+        address: wallet.address,
+        balance: 0,
+      };
+      getEthereumBalance(ethereumWallet.address);
+      return ethereumWallet;
     case "polygon":
       const endpointUrl = `https://polygon-mumbai.infura.io/v3/${process.env.REACT_APP_QUICK_NODE_KEY}/`;
       const polygonProvider = new ethers.InfuraProvider("polygon", endpointUrl);
       wallet = new ethers.Wallet(seedPhrase, polygonProvider);
       return wallet;
+    case "solana":
+      const mnemonic = seedPhrase;
+      // 니모닉을 시드로 변환
+      const seed = bip39.mnemonicToSeedSync(mnemonic, "");
+
+      // seed를 사용하여 키쌍 생성
+      const keypair = Keypair.fromSeed(seed.slice(0, 32));
+
+      // 공개키를 Base58 형식의 문자열로 인코딩
+      const walletAddress = keypair.publicKey.toBase58();
+      // console.log("keypair", keypair.publicKey, keypair.publicKey.toBase58());
+      // const pubKey = new SolanaWeb3.PublicKey(keypair.publicKey);
+      // console.log("pubKey", pubKey);
+      const solanaWallet = {
+        network: "solana",
+        name: "",
+        pubKey: keypair.publicKey,
+        address: walletAddress,
+        balance: 0,
+      };
+
+      return solanaWallet;
+    case "bitcoin":
+      // mnemonic generation for deterministic keys
+      // const bitcoinSeed = bip39.mnemonicToSeedSync(seedPhrase);
+      let words = bip39.generateMnemonic();
+      const sed = bip39.mnemonicToSeedSync(words);
+      let bip32 = BIP32Factory(ecc);
+      // let root = bip32.fromSeed(bitcoinSeed);
+      let root = bip32.fromSeed(sed, bitcoin.networks.testnet);
+      let account = root.derivePath("m/44'/0'/0'/0/0");
+
+      let node = account.derive(0).derive(0);
+
+      let btcAddress = bitcoin.payments.p2pkh({
+        pubkey: node.publicKey,
+        network: bitcoin.networks.testnet,
+      }).address;
+
+      const bitcoinWallet = {
+        network: "bitcoin",
+        name: "",
+        address: btcAddress,
+        balance: 0,
+      };
+
+      getBitcoinBalance("moigp5SXJZoTDd7PTZ7kquNYkPQHY4vEVH");
+      getBitcoinBalance(btcAddress!);
+      // console.log(`
+
+      // Wallet generated:
+
+      // - Address : ${btcAddress},
+      // - Key : ${node.toWIF()},
+      // - Mnemonic : ${seedPhrase}
+
+      // // `);
+
+      return bitcoinWallet;
   }
 };
+
 
 export const createChildWallet = (
   network: string,
@@ -151,4 +223,56 @@ export const createChildWallet = (
       );
       return childWallet;
   }
+};
+
+export const getEthereumBalance = async (address?: string) => {
+  const provider = ethers.getDefaultProvider("sepolia");
+  const balance = await provider.getBalance(
+    "0x087b7A9645cc78873b11A40aDe7Bb5EDF3dAFb8d"
+  );
+
+  const 잔액 = ethers.formatEther(balance);
+  console.log("이더리움 잔액", balance, "wei", 잔액, "ETH");
+  return balance;
+};
+
+export const getBitcoinBalance = (address: string) => {
+  // Base58Check 형식의 문자열(주소)을 해석(디코딩)
+  // Base58Check : 주소와 체크섬을 사용하여 오류 검출을 수행하는 인코딩 방식
+  const decodedAddress = bitcoin.address.fromBase58Check(address);
+  console.log(
+    "P2PKH 유효성 확인해보자",
+    decodedAddress,
+    decodedAddress.version,
+    bitcoin.networks.testnet
+  );
+  // 주소가 네트워크(테스트넷)의 주소인지 확인
+  // P2PKH 유효성 확인
+  if (decodedAddress.version === bitcoin.networks.testnet.pubKeyHash) {
+    console.log("유효함");
+  }
+
+  axios
+    .get(`https://api.blockcypher.com/v1/btc/test3/addrs/${address}/balance`)
+    .then((response) => {
+      console.log("response", response);
+      // const utxos = response.data;
+      // const balance = utxos.reduce(
+      //   (total: any, utxo: any) => total + utxo.value,
+      //   0
+      // );
+      // console.log("잔액:", balance);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const getSolanaBalance = async (pubKey: SolanaWeb3.PublicKey) => {
+  console.log("솔라나 잔액조회", pubKey);
+  const solanaWeb3 = new SolanaWeb3.Connection('https://api.testnet.solana.com');
+  const lamports = await solanaWeb3.getBalance(pubKey);
+  console.log(lamports);
+  console.log(`${lamports / SolanaWeb3.LAMPORTS_PER_SOL} SOL`);
+  // return lamports;
 };
